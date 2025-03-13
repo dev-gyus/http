e2d2653ad15af164e6658ef04862b98084e2e7dd
GyuHyeong <cjsworbehd13@gmail.com>
2025-03-13 19:18:50 +0900

    1. application/json 컨텐츠타입 응답 데이터 받는 기능 추가

---
+++ ch06/public/script.js
@@ -0,0 +1,40 @@
+const init = () => {
+  const loginForm = document.getElementById("login-form");
+  if (!loginForm) throw "no login-form";
+
+  loginForm.addEventListener("submit", handleSubmit);
+};
+
+const handleSubmit = async (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  // const urlSearchParams = new URLSearchParams();
+  // urlSearchParams.append("email", formData.get('email'));
+  // urlSearchParams.append("password", formData.get('password'));
+  const jsonData = {
+    email: formData.get("email"),
+    password: formData.get("password"),
+  }
+  try {
+    const res = await fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // body: formData
+      // body: urlSearchParams.toString(),
+      body: JSON.stringify(jsonData),
+    });
+    if (!res.ok) {
+      alert('API Error');
+      return;
+    }
+    // console.log(await res.text());
+    console.log(await res.json());
+    alert("Success");
+  } catch (error) {
+    alert("Network error");
+  }
+}
+
+document.addEventListener('DOMContentLoaded', init);
---
+++ ch06/server.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const path = require('path');
+const static = require('../shared/serve-static');
+
+const loginHandler = (req, res) => {
+  let body = '';
+  req.on('data', chunk => {
+    body = body + chunk.toString();
+  });
+  req.on('end', () => {
+    const {email, password} = JSON.parse(body);
+    const authenticated = email === "myemail" && password === "mypassword";
+    res.statusCode = authenticated ? 200 : 401;
+    res.setHeader("Content-Type", "application/json");
+    res.write(JSON.stringify({authenticated}));
+    res.end();
+  })
+}
+
+const handler = (req, res) => {
+  const { pathname} = new URL(req.url, `http://${req.headers.host}`);
+  if (pathname === '/login') {
+    loginHandler(req, res);
+    return;
+  }
+  static(path.join(__dirname, 'public'))(req, res);
+};
+const server = http.createServer(handler);
+server.listen(3000, () => {console.log('Server started on port 3000')});
